33f10a4af910a447043120df0a8dc7de633b60e7
Ian Thompson <quornian@gmail.com>
2024-02-24 20:02:10 -0500

    Switch "remove" to "keep" on template-button

--- widgets/template-button/macro.query.clj
+++ widgets/template-button/macro.query.clj
@@ -1,14 +1,14 @@
-;; Usage: {{template-button <template-name>,<remove-on-click>}}
+;; Usage: {{template-button <template-name>,<keep-on-click>}}
 ;; where
 ;;   <template-name> is the name of the template to insert when clicked.
-;;   <remove-on-click> is the word "remove" if the button is to be removed on click.
+;;   <keep-on-click> is the word "keep" if the button is to be kept on click.
 ;; Examples:
 ;;     {{template-button A Template}}
-;;     {{template-button My Other Template, remove}}
+;;     {{template-button My Other Template, keep}}
 {:inputs [:current-block]
  :query [:find ?block-uuid
          :in $ ?block
          :where [?block :block/uuid ?block-uuid]]
  :result-transform (fn [[block-uuid]]
-                     [block-uuid "$1" (= "$2" "remove")])
+                     [block-uuid "$1" (= "$2" "keep")])
  :view :template-button}
--- widgets/template-button/query-view.clj
+++ widgets/template-button/query-view.clj
@@ -1,14 +1,14 @@
-;; Creates a button from a block UUID, template name and remove flag
+;; Creates a button from a block UUID, template name and keep flag
 ;; On click, appends the specified template after the given block,
-;; optionally removing the original block (if the remove flag is set)
-(fn [[block-uuid template-name remove-on-click?]]
+;; removing the original block or keeling it if the "keep" flag is set
+(fn [[block-uuid template-name keep-on-click?]]
   [:button
    {:class "lsw-template-button"
     :on-click
     (fn [_]
       (if (call-api "exist_template" template-name)
         (do (call-api "insert_template" block-uuid template-name)
-            (if remove-on-click?
+            (if (not keep-on-click?)
               (call-api "remove_block" block-uuid)))
         (call-api "show_msg"
                   (str "Template not found:\n" template-name) :error)))}

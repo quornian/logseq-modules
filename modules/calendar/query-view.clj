1d4ea1d7bcd3ddfb92660eaaed1148c3980feeec
Ian Thompson <quornian@gmail.com>
2024-04-18 20:40:13 -0400

    Support all date formats in the settings dialog, including the default!
    If the journal-format was not set the calendar failed to render. And many
    formats did not work.

--- modules/calendar/query-view.clj
+++ modules/calendar/query-view.clj
@@ -9,7 +9,7 @@
         ;; User preferences
         ;; :journal/page-title-format "yyyy-MM-dd EEEE"
         config (js->clj (call-api "get_current_graph_configs"))
-        journal-format (get config "journal/page-title-format")
+        journal-format (get config "journal/page-title-format" "MMM do, yyyy")
 
         ;; Extract a YYYYMMDD number into a [YYYY MM DD] sequence
         ymd->seq (fn [ymd] [(÷ ymd 10000) (÷ (% ymd 10000) 100) (% ymd 100)])
@@ -34,10 +34,6 @@
                 y (+ (÷ b 1461) -4716 (÷ (- 12 -2 m) 12))]
             (+ (* 10000 y) (* 100 m) d)))
 
-        ;; Date offset functions
-        ymd- (fn [ymd sub] (jdn->ymd (- (ymd->jdn ymd) sub)))
-        ymd+ (fn [ymd sub] (jdn->ymd (+ (ymd->jdn ymd) sub)))
-
         ;; Weekday number with zero denoting Sunday
         jdn->wd (fn [jdn] (% (+ jdn 1) 7))
         ymd->wd (fn [ymd] (jdn->wd (ymd->jdn ymd)))
@@ -48,30 +44,39 @@
         month ["December" "January" "February" "March" "April" "May" "June"
                "July" "August" "September" "October" "November" "December"]
 
+        ;; Ordinal suffix for a given day of the month: 1st 2nd...11th...31st
+        ordinal-d
+        (fn [d] (let [r (% d 10)] (if (or (> r 3) (and (> d 10) (< d 14)))
+                                    "th" (nth ["th" "st" "nd" "rd"] r))))
+
         ;; Format dates as strings
+        ;; A subset of https://cljdoc.org/d/com.andrewmcveigh/cljs-time/0.5.2/api/cljs-time.format#formatters
         format-date
         (fn [ymd fmt]
+          ;; (log "(format-date " ymd " " fmt ")")
           (let [j (ymd->jdn ymd)
                 [y m d] (ymd->seq ymd)]
             (clojure.string/replace
              fmt
-             (re-pattern "%[YmdA]|yyyy|MM|dd|EEEE")
+             (re-pattern "y+|M+|d+|E+|o")
              (fn [match]
                (case match
-                 "%Y" (str y)
-                 "yyyy" (str y)
-                 "%m" (str (when (< m 10) "0") m)
+                 ("y" "yy" "yyy" "yyyy") (str y)
+                 "M" (str m)
                  "MM" (str (when (< m 10) "0") m)
-                 "%d" (str (when (< d 10) "0") d)
+                 "MMM" (subs (month m) 0 3)
+                 "MMMM" (month m)
+                 "d" (str d)
                  "dd" (str (when (< d 10) "0") d)
-                 "%A" (weekday (ymd->wd ymd))
+                 ("E" "EE" "EEE") (subs (weekday (ymd->wd ymd)) 0 3)
                  "EEEE" (weekday (ymd->wd ymd))
+                 "o" (ordinal-d d)
                  match)))))
 
         ;; Properties for use in an [:a ...] element linking to a block or page
         page-link
         (fn page-link [page-name]
-          {:href (str "#/page/" page-name)
+          {:href (str "#/page/" (clojure.string/replace page-name "/" "%2F"))
            :on-click
            (fn [e] (if (aget e "shiftKey")
                      (call-api "open_in_right_sidebar"
@@ -84,8 +89,15 @@
                  (fn [e] (if (aget e "shiftKey")
                            (call-api "open_in_right_sidebar" uuid)))))]
 
-    (log "Calendar settings:", (clj->js {:highlight-overdue highlight-overdue
-                                         :show-weekends show-weekends}))
+    (log "[Calendar Module]"
+         "\nCalendar properties:"
+         (clj->js {:calendar/highlight-overdue highlight-overdue
+                   :calendar/show-weekends show-weekends})
+         "\nUser configuration:"
+         (clj->js {:journal-format journal-format})
+         "\nDebug:"
+         (clj->js {:today today :today-formatted (format-date today journal-format)}))
+
     ;; The main view
     [:div.lsm-calendar
      {:class [(when (not show-weekends) "lsm-hide-weekends")]}
